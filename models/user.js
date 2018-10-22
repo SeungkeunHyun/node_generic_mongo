449537d7828fe416f72da914c6236762dfa1cbce
unknown <mysuccess@daum.net>
2018-10-22 11:33:17 +0900

    replaced indentation from spaces to tab

--- models/user.js
+++ models/user.js
@@ -5,67 +5,67 @@ const _ = require("lodash");
 const crypto = require("crypto");
 
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 1,
-    unique: true,
-    validate: {
-      validator: validator.isEmail,
-      message: "{VALUE} is not a valid email"
-    }
-  },
-  name: {
-    type: String,
-    required: true, 
-    trim: true,
-    minlength: 3
-  },
-  password: {
-    type: String,
-    require: true,
-    minlength: 6
-  },
-  salt: {
-    type: String
-  }
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 1,
+		unique: true,
+		validate: {
+			validator: validator.isEmail,
+			message: "{VALUE} is not a valid email"
+		}
+	},
+	name: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 3
+	},
+	password: {
+		type: String,
+		require: true,
+		minlength: 6
+	},
+	salt: {
+		type: String
+	}
 });
 
-UserSchema.methods.toJSON = function() {
-  return {
-    _id: this._id,
-    email: this.email,
-    token: this.generateJWT()
-  };
+UserSchema.methods.toJSON = function () {
+	return {
+		_id: this._id,
+		email: this.email,
+		token: this.generateJWT()
+	};
 };
 
-UserSchema.methods.setPassword = function() {
-  console.log("This", this);
-  this.salt = crypto.randomBytes(16).toString('hex');
-  this.password = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
+UserSchema.methods.setPassword = function () {
+	console.log("This", this);
+	this.salt = crypto.randomBytes(16).toString('hex');
+	this.password = crypto.pbkdf2Sync(this.password, this.salt, 1000, 64, 'sha512').toString('hex');
 }
 
-UserSchema.methods.validPassword = function(password){
-  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
-  return this.password === hash;
+UserSchema.methods.validPassword = function (password) {
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+	return this.password === hash;
 };
 
-UserSchema.methods.generateJWT = function() {
-  var expiry = new Date();
-  expiry.setDate(expiry.getDate() + 7);
-  return jwt.sign({
-    _id: this._id,
-    email: this.email, 
-    name: this.name, 
-    exp: parseInt(expiry.getTime() / 1000)
-  }, process.env.JWT_SECRET);
+UserSchema.methods.generateJWT = function () {
+	var expiry = new Date();
+	expiry.setDate(expiry.getDate() + 7);
+	return jwt.sign({
+		_id: this._id,
+		email: this.email,
+		name: this.name,
+		exp: parseInt(expiry.getTime() / 1000)
+	}, process.env.JWT_SECRET);
 };
 
-UserSchema.pre('save', function(next) {
-  var user = this;
-  user.setPassword();
-  next();
+UserSchema.pre('save', function (next) {
+	var user = this;
+	user.setPassword();
+	next();
 });
 
 var User = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -1,90 +1,97 @@
-require('./config/config.js');
-const app = require('express')();
-const passport = require('./config/passport');
-const session = require('express-session');
-const authMW = require('./middlewares/authenticate.js');
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({extended: false});
-var modelRunner = require('./controllers/run-model');
-
+require("./config/config.js");
+const app = require("express")();
+const passport = require("./config/passport");
+const session = require("express-session");
+const authMW = require("./middlewares/authenticate.js");
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded({
+	extended: false
+});
+var modelRunner = require("./controllers/run-model");
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(bodyParser.json());
 
-app.get('/', urlencodedParser, (req, res)=> {
-    res.send("<h1>Welcome</h1>");
-})
-
-app.get('/login', authMW.isLoggedIn, (req, res) => {
-    if(req.isAuthenticated()) {
-        res.send("Use this api with current auth");
-        return;
-    } 
-    res.send("<h1>Please login to use this service</h1>");
+app.get("/", urlencodedParser, (req, res) => {
+	res.send("<h1>Welcome</h1>");
 });
 
-app.get('/profile', authMW.isLoggedIn, (req, res) => {
-    if(req.isAuthenticated()) {
-        res.send(req.user);
-        return;
-    } 
-    res.send(`<h1>Hi, ${req.user.name}</h1>`);
+app.get("/login", authMW.isLoggedIn, (req, res) => {
+	if (req.isAuthenticated()) {
+		res.send("Use this api with current auth");
+		return;
+	}
+	res.send("<h1>Please login to use this service</h1>");
 });
 
-app.post('/login', passport.authenticate('local-signin', {session:false}), (req,res) => {
-    res.set('x-auth', req.user.token);
-    res.status(200).json(req.user);
+app.get("/profile", authMW.isLoggedIn, (req, res) => {
+	if (req.isAuthenticated()) {
+		res.send(req.user);
+		return;
+	}
+	res.send(`<h1>Hi, ${req.user.name}</h1>`);
 });
 
+app.post(
+	"/login",
+	passport.authenticate("local-signin", {
+		session: false
+	}),
+	(req, res) => {
+		res.set("x-auth", req.user.token);
+		res.status(200).json(req.user);
+	}
+);
 
-app.post('/api/:col', authMW.verifyToken, (req, res) => {
-    if(!req.body)
-        return res.sendStatus(400);
-    console.log(req.params.col);
-    var newRow = modelRunner.processRequest(req, res);
-    newRow.then(nr=> {
-        res.send(nr);
-         return;
-    }).catch(ex => {
-        console.log(ex);
-        res.status(500).send(ex.message);
-    });
+app.post("/api/:col", authMW.verifyToken, (req, res) => {
+	if (!req.body) return res.sendStatus(400);
+	console.log(req.params.col);
+	var newRow = modelRunner.processRequest(req, res);
+	newRow
+		.then(nr => {
+			res.send(nr);
+			return;
+		})
+		.catch(ex => {
+			console.log(ex);
+			res.status(500).send(ex.message);
+		});
 });
 
-app.get('/api/:col', authMW.verifyToken, (req, res) => {
-    if(!req.params.col) 
-        return res.sendStatus(400);
-    let msg = `get all documents of collection: ${req.params.col}`;
-    var query = modelRunner.processRequest(req, res);
-    query.exec((err, docs) => {
-        console.log("entered then", err, docs);
-        if(err) {
-            res.sendStatus(400);
-            return;
-        }
-        console.log(docs);
-        res.send(docs);
-    });
+app.get("/api/:col", authMW.verifyToken, (req, res) => {
+	if (!req.params.col) return res.sendStatus(400);
+	let msg = `get all documents of collection: ${req.params.col}`;
+	var query = modelRunner.processRequest(req, res);
+	query.exec((err, docs) => {
+		console.log("entered then", err, docs);
+		if (err) {
+			res.sendStatus(400);
+			return;
+		}
+		console.log(docs);
+		res.send(docs);
+	});
 });
 
-app.get('/api/:col/:id', authMW.verifyToken, (req, res) => {
-    if(!req.params.col) 
-        return res.sendStatus(400);
-    let msg = `get a record of collection: ${req.params.col}, id: ${req.params.id}`;
-    var query = modelRunner.processRequest(req, res);
-    console.log('query', query);
-    query.exec((err, docs) => {
-        console.log("entered then");
-        if(err) {
-            res.sendStatus(400);
-            return;
-        }
-        res.send(docs);
-    });
+app.get("/api/:col/:id", authMW.verifyToken, (req, res) => {
+	if (!req.params.col) return res.sendStatus(400);
+	let msg = `get a record of collection: ${req.params.col}, id: ${
+    req.params.id
+  }`;
+	var query = modelRunner.processRequest(req, res);
+	console.log("query", query);
+	query.exec((err, docs) => {
+		console.log("entered then");
+		if (err) {
+			res.sendStatus(400);
+			return;
+		}
+		res.send(docs);
+	});
 });
 
 var port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`node is listening on ${port}`);
+	console.log(`node is listening on ${port}`);
 });
